ddc8236597b30add3d21ca9ecc3e19006df7c3ae
Higor Dias Zuqueto <higordiaszuqueto@gmail.com>
2023-02-08 13:39:04 -0300

    1.2

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { View, Text, ScrollView } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-yar-beginning'
 
@@ -11,6 +12,9 @@ const minimumSumaryDatesSizes = 18 * 7;
 const amountOfDaysToFill = minimumSumaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+
+  const { navigate } = useNavigation()
+
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
@@ -38,6 +42,7 @@ export function Home() {
             datesFromYearStart.map(date => (
               <HabitDay
                 key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
             ))
           }
